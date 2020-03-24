856be6f1047f9dc5cad8a1ab00daa32f564b361f
Gurjot <1313gurjotsingh@gmail.com>
2020-03-25 02:03:52 +0530

    changes

---
+++ CNN Vision/code/softmax.py
@@ -0,0 +1,116 @@
+
+import numpy as np
+from layers import *
+
+class SoftmaxClassifier(object):
+  """
+  A logistic regression model with optional hidden layers.
+  
+  Note that this class does not implement gradient descent; instead, it
+  will interact with a separate Solver object that is responsible for running
+  optimization.
+
+  The learnable parameters of the model are stored in the dictionary
+  self.params that maps parameter names to numpy arrays.
+  """
+  
+  def __init__(self, input_dim=28*28, hidden_dim=100, num_classes=10,
+                 weight_scale=1e-3, reg=0.0):
+    """
+    Initialize a new network.
+
+    Inputs:
+    - input_dim: An integer giving the size of the input
+    - hidden_dim: An integer giving the size of the hidden layer
+    - weight_scale: Scalar giving the standard deviation for random
+      initialization of the weights.
+    - reg: Scalar giving L2 regularization strength.
+    """
+    self.params = {}
+    self.reg = reg
+    self.hidden_dim = hidden_dim
+    ############################################################################
+    # TODO: Initialize the weights and biases of the model. Weights            #
+    # should be initialized from a Gaussian with standard deviation equal to   #
+    # weight_scale, and biases should be initialized to zero. All weights and  #
+    # biases should be stored in the dictionary self.params, with first layer  #
+    # weights and biases using the keys 'W1' and 'b1' and second layer weights #
+    # and biases (if any) using the keys 'W2' and 'b2'.                        #
+    ############################################################################
+    if hidden_dim is not None:
+        self.params['W1'] = np.random.normal(0, weight_scale, (input_dim, hidden_dim))
+        self.params['b1'] = np.zeros((1,hidden_dim)) 
+        self.params['W2'] = np.random.normal(0, weight_scale, (num_classes, hidden_dim))
+        self.params['b2'] = np.zeros((1, num_classes))  
+    else:
+        self.params['W1'] = np.random.normal(0, weight_scale, (input_dim, num_classes))
+        self.params['b1'] = np.zeros((1, num_classes))
+    ############################################################################
+    #                             END OF YOUR CODE                             #
+    ############################################################################
+
+  def loss(self, X, y=None):
+    """
+    Compute loss and gradient for a minibatch of data.
+
+    Inputs:
+    - X: Array of input data of shape (N, D)
+    - y: Array of labels, of shape (N,). y[i] gives the label for X[i].
+
+    Returns:
+    If y is None, then run a test-time forward pass of the model and return:
+    - scores: Array of shape (N,) where scores[i] represents the logit for X[i]
+    
+    If y is not None, then run a training-time forward and backward pass and
+    return a tuple of:
+    - loss: Scalar value giving the loss
+    - grads: Dictionary with the same keys as self.params, mapping parameter
+      names to gradients of the loss with respect to those parameters.
+    """  
+    scores = None
+    ############################################################################
+    # TODO: Implement the forward pass for the model, computing the            #
+    # scores for X and storing them in the scores variable.                    #
+    ############################################################################
+    if 'W2' in self.params:
+        # z1 = np.matmul(X,self.params['W1']) + np.matmul(np.ones((np.shape(X)[0],1)), self.params['b1'])
+        z1, _ = fc_forward(X, self.params['W1'], self.params['b1'])
+        a1_relu, relu_cache = relu_forward(z1)
+        z2, _ = fc_forward(a1_relu, np.transpose(self.params['W2']), self.params['b2'])
+        scores = z2
+    else:
+        z1 = np.matmul(X, self.params['W1']) + self.params['b1'] 
+        scores = z1    
+    ############################################################################
+    #                             END OF YOUR CODE                             #
+    ############################################################################
+
+    # If y is None then we are in test mode so just return scores
+    if y is None:
+      return scores
+    
+    loss, grads = 0, {}
+    ############################################################################
+    # TODO: Implement the backward pass for the model. Store the loss          #
+    # in the loss variable and gradients in the grads dictionary. Compute data #
+    # loss and make sure that grads[k] holds the gradients for self.params[k]. #
+    # Don't forget to add L2 regularization.                                   #
+    #                                                                          #
+    ############################################################################
+    loss, dscores = softmax_loss(scores, y)
+    if 'W2' in self.params:
+        loss += 0.5 * self.reg * (np.linalg.norm(self.params['W1'])**2 + np.linalg.norm(self.params['W2'])**2)
+        grads['W2'] = np.dot(np.transpose(dscores), z1) + self.reg * self.params['W2']
+        grads['b2'] = np.sum(dscores)
+        relu_backward_out = relu_backward(np.dot(dscores, self.params['W2']), relu_cache)
+        grads['W1'] = np.dot(X.T, relu_backward_out) + self.reg * self.params['W1']
+        grads['b1'] = np.dot(np.ones((1, len(y))), relu_backward_out) 
+    else:
+        loss += 0.5 * self.reg * (np.linalg.norm(self.params['W1'])**2)
+        grads['W1'] = np.dot(np.transpose(X), dscores) + self.reg * self.params['W1']
+        grads['b1'] = np.sum(dscores)
+    ############################################################################
+    #                             END OF YOUR CODE                             #
+    ############################################################################
+
+    return loss, grads
---
+++ CNN Vision/code/svm.py
@@ -0,0 +1,182 @@
+import numpy as np
+
+from layers import *
+
+np.random.seed(1)
+
+class SVM(object):
+  """
+  A logistic regression model with optional hidden layers.
+  
+  Note that this class does not implement gradient descent; instead, it
+  will interact with a separate Solver object that is responsible for running
+  optimization.
+
+  The learnable parameters of the model are stored in the dictionary
+  self.params that maps parameter names to numpy arrays.
+  """
+  
+  def __init__(self, input_dim=100, hidden_dim=None, weight_scale=1e-3, reg=0.0):
+    """
+    Initialize a new network.
+
+    Inputs:
+    - input_dim: An integer giving the size of the input
+    - hidden_dim: An integer giving the size of the hidden layer
+    - weight_scale: Scalar giving the standard deviation for random
+      initialization of the weights.
+    - reg: Scalar giving L2 regularization strength.
+    """
+    self.params = {}
+    self.reg = reg
+    
+    ############################################################################
+    # TODO: Initialize the weights and biases of the model. Weights            #
+    # should be initialized from a Gaussian with standard deviation equal to   #
+    # weight_scale, and biases should be initialized to zero. All weights and  #
+    # biases should be stored in the dictionary self.params, with first layer  #
+    # weights and biases using the keys 'W1' and 'b1' and second layer weights #
+    # and biases (if any) using the keys 'W2' and 'b2'.                        #
+    ############################################################################
+    if hidden_dim is not None:
+        self.params['W1'] = np.random.normal(0, weight_scale, (input_dim,hidden_dim))
+        self.params['b1'] = np.zeros((1,hidden_dim)) 
+        self.params['W2'] = np.random.normal(0, weight_scale, (1,hidden_dim))
+        self.params['b2'] = 0  
+    else:
+        self.params['W1'] = np.random.normal(0, weight_scale, (input_dim,1))
+        self.params['b1'] = 0
+    ############################################################################
+    #                             END OF YOUR CODE                             #
+    ############################################################################
+
+
+  # def loss(self, X, y=None):
+  #   """
+  #   Compute loss and gradient for a minibatch of data.
+
+  #   Inputs:
+  #   - X: Array of input data of shape (N, D)
+  #   - y: Array of labels, of shape (N,). y[i] gives the label for X[i].
+
+  #   Returns:
+  #   If y is None, then run a test-time forward pass of the model and return:
+  #   - scores: Array of shape (N,) where scores[i] represents the logit for X[i]
+    
+  #   If y is not None, then run a training-time forward and backward pass and
+  #   return a tuple of:
+  #   - loss: Scalar value giving the loss
+  #   - grads: Dictionary with the same keys as self.params, mapping parameter
+  #     names to gradients of the loss with respect to those parameters.
+  #   """  
+  #   scores = None
+  #   ############################################################################
+  #   # TODO: Implement the forward pass for the model, computing the            #
+  #   # scores for X and storing them in the scores variable.                    #
+  #   ############################################################################
+  #   if 'W2' in self.params:
+  #       logit1 = np.matmul(X,self.params['W1']) + np.matmul(np.ones((np.shape(X)[0],1)), self.params['b1'])
+  #       logit1_relu, relu_cache = relu_forward(logit1)
+  #       logit2 = np.matmul(logit1_relu, np.transpose(self.params['W2'])) + self.params['b2']
+  #       scores = logit2
+  #   else:
+  #       logit1 = np.matmul(X, self.params['W1']) + self.params['b1'] 
+  #       scores = logit1    
+  #   ############################################################################
+  #   #                             END OF YOUR CODE                             #
+  #   ############################################################################
+
+  #   # If y is None then we are in test mode so just return scores
+  #   if y is None:
+  #     return scores.reshape(-1)
+    
+  #   loss, grads = 0, {}
+  #   ############################################################################
+  #   # TODO: Implement the backward pass for the model. Store the loss          #
+  #   # in the loss variable and gradients in the grads dictionary. Compute data #
+  #   # loss and make sure that grads[k] holds the gradients for self.params[k]. #
+  #   # Don't forget to add L2 regularization.                                   #
+  #   #                                                                          #
+  #   ############################################################################
+  #   loss, grad1 = svm_loss(scores, y)
+  #   if 'W2' in self.params:
+  #       grads['W2'] = np.matmul(np.transpose(grad1) , logit1)
+  #       grads['b2'] = np.sum(grad1)
+  #       # Pass through relu backward
+  #       grads['W1'] = np.matmul(np.transpose(X), relu_backward(np.matmul(grad1, self.params['W2']), relu_cache))
+  #       grads['b1'] = np.matmul(np.ones((1,len(y))), relu_backward(np.matmul(grad1, self.params['W2']), relu_cache)) 
+  #   else:
+  #       grads['W1'] = np.matmul(np.transpose(X) , grad1)
+  #       grads['b1'] = np.sum(grad1)
+  #   ############################################################################
+  #   #                             END OF YOUR CODE                             #
+  #   ############################################################################
+
+  #   return loss, grads
+
+  def loss(self, X, y=None):
+    """
+    Compute loss and gradient for a minibatch of data.
+
+    Inputs:
+    - X: Array of input data of shape (N, D)
+    - y: Array of labels, of shape (N,). y[i] gives the label for X[i].
+
+    Returns:
+    If y is None, then run a test-time forward pass of the model and return:
+    - scores: Array of shape (N,) where scores[i] represents the logit for X[i]
+    
+    If y is not None, then run a training-time forward and backward pass and
+    return a tuple of:
+    - loss: Scalar value giving the loss
+    - grads: Dictionary with the same keys as self.params, mapping parameter
+      names to gradients of the loss with respect to those parameters.
+    """  
+    scores = None
+    ############################################################################
+    # TODO: Implement the forward pass for the model, computing the            #
+    # scores for X and storing them in the scores variable.                    #
+    ############################################################################
+    if 'W2' in self.params:
+        # z1 = np.matmul(X,self.params['W1']) + np.matmul(np.ones((np.shape(X)[0],1)), self.params['b1'])
+        z1, _ = fc_forward(X, self.params['W1'], self.params['b1'])
+        a1_relu, relu_cache = relu_forward(z1)
+        z2, _ = fc_forward(a1_relu, np.transpose(self.params['W2']), self.params['b2'])
+        scores = z2
+    else:
+        z1 = np.matmul(X, self.params['W1']) + self.params['b1'] 
+        scores = z1    
+    ############################################################################
+    #                             END OF YOUR CODE                             #
+    ############################################################################
+
+    # If y is None then we are in test mode so just return scores
+    if y is None:
+      return scores.reshape(-1)
+    
+    loss, grads = 0, {}
+    ############################################################################
+    # TODO: Implement the backward pass for the model. Store the loss          #
+    # in the loss variable and gradients in the grads dictionary. Compute data #
+    # loss and make sure that grads[k] holds the gradients for self.params[k]. #
+    # Don't forget to add L2 regularization.                                   #
+    #                                                                          #
+    ############################################################################
+    loss, dscores = svm_loss(scores, y)
+    dscores = dscores.reshape((-1, 1))
+    if 'W2' in self.params:
+        loss += 0.5 * self.reg * (np.linalg.norm(self.params['W1'])**2 + np.linalg.norm(self.params['W2'])**2)
+        grads['W2'] = np.dot(np.transpose(dscores), z1) + self.reg * self.params['W2']
+        grads['b2'] = np.sum(dscores)
+        relu_backward_out = relu_backward(np.dot(dscores, self.params['W2']), relu_cache)
+        grads['W1'] = np.dot(X.T, relu_backward_out) + self.reg * self.params['W1']
+        grads['b1'] = np.dot(np.ones((1, len(y))), relu_backward_out) 
+    else:
+        loss += 0.5 * self.reg * (np.linalg.norm(self.params['W1'])**2)
+        grads['W1'] = np.dot(np.transpose(X), dscores) + self.reg * self.params['W1']
+        grads['b1'] = np.sum(dscores)
+    ############################################################################
+    #                             END OF YOUR CODE                             #
+    ############################################################################
+
+    return loss, grads
